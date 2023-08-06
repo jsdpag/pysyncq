f30da5a186939223379c5718949162730c6d4b5c
Jackson Smith <jackson.smith@esi-frankfurt.de>
2023-08-06 18:47:11 +0200

    Added more package infrastructure.

---
+++ pysyncq/header.py
@@ -0,0 +1,17 @@
+
+'''
+Collects important constants together into a common namespace.
+'''
+
+#--- IMPORT BLOCK ---#
+
+import  resource
+
+
+#--- GLOBALS ---#
+
+# Page size in memory e.g. 4096 bytes.
+# Default size of the shared memory.
+defsize = resource.getpagesize( )
+
+
