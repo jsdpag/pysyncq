c7d8763918be624fe4e890e7d56047de3034364a
Jackson Smith <22416446+jsdpag@users.noreply.github.com>
2023-08-18 17:19:41 +0200

    More pypi docs.

--- setup.py
+++ setup.py
@@ -4,7 +4,13 @@ Install the Python Synchronisation Queue - pysyncqpip
 '''
 
 from setuptools import setup , find_packages
+from pathlib import Path
 
+# read the contents of your README file
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+# Setup parameters.
 setup(  name = 'pysyncq' ,
      version = '0.0.0' ,
  description = 'Python based time/data synchronisation queue' ,
@@ -12,5 +18,8 @@ setup(  name = 'pysyncq' ,
       author = 'Jackson Smith' ,
      license = 'GPL' ,
    packages = find_packages( ) ,
-   zip_safe = False )
+   zip_safe = False ,
+   long_description=long_description,
+   long_description_content_type='text/markdown'
+    )
 
