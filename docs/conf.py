49073081d77f4a5c58c4bd2bcea7dfe78b81aa40
Jackson Smith <22416446+jsdpag@users.noreply.github.com>
2023-08-18 17:12:51 +0200

    Attempt to resove RtD build fail.

--- docs/conf.py
+++ docs/conf.py
@@ -36,4 +36,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme_path = ["_themes", ]
+#html_static_path = ['_static']
