b76a8af04e1afb581e6e1a0028f63fd43726870f
Jackson Smith <22416446+jsdpag@users.noreply.github.com>
2023-08-18 17:06:19 +0200

    Attempt to solve ReadTheDocs build failure.

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ release = '0.0.0'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [ 'sphinx.ext.autodoc' , 'sphinx.ext.viewcode', 
-               'sphinx.ext.napoleon']
+               'sphinx.ext.napoleon' , 'sphinx_rtd_theme' ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
